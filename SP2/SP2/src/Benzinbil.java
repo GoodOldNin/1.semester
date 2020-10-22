78c56236b36ff5b6507110d1d010ec413474296c
GoodOldNin <71062853+GoodOldNin@users.noreply.github.com>
2020-10-22 09:55:24 +0200

    pewpew

---
+++ SP2/SP2/src/Benzinbil.java
@@ -0,0 +1,67 @@
+class Benzinbil extends Bil
+{
+    public int oktantal;
+    public int kmPrL;
+
+    public Benzinbil(int regNr, int mærke, int model, int årgang, int antalDøre, int oktantal, int kmPrL) {
+        super(regNr, mærke, model, årgang, antalDøre);
+        this.oktantal = oktantal;
+        this.kmPrL = kmPrL;
+    }
+
+    public int getOktantal() {
+        return oktantal;
+    }
+
+    public void setOktantal(int oktantal) {
+        this.oktantal = oktantal;
+    }
+
+    public int getKmPrL() {
+        return kmPrL;
+    }
+
+    public void setKmPrL(int kmPrL) {
+        this.kmPrL = kmPrL;
+    }
+
+    @Override
+    public String toString() {
+        return "Benzinbil{" +
+                "regNr=" + regNr +
+                ", mærke=" + mærke +
+                ", model=" + model +
+                ", årgang=" + årgang +
+                ", antalDøre=" + antalDøre +
+                ", oktantal=" + oktantal +
+                ", kmPrL=" + kmPrL +
+                '}';
+    }
+
+    @Override
+    public double beregnGrønEjerafgift()
+    {
+        if ( kmPrL < 5)
+        {
+            return 10470;
+        }
+        if (5 <= kmPrL && kmPrL < 10)
+        {
+            return 5500;
+        }
+        if (10 <= kmPrL && kmPrL < 15)
+        {
+            return 2340;
+        }
+        if (15 <= kmPrL && kmPrL < 20)
+        {
+            return 1050;
+        }
+        if (20 <= kmPrL && kmPrL < 50)
+        {
+            return 330;
+        }
+        return 0;
+
+    }
+}
---
+++ SP2/SP2/src/Dieselbil.java
@@ -0,0 +1,45 @@
+class Dieselbil extends Bil
+{
+    public boolean harPartikelFilter;
+    public int kmPrL;
+
+    public Dieselbil(int regNr, int mærke, int model, int årgang, int antalDøre, boolean harPartikelFilter, int kmPrL) {
+        super(regNr, mærke, model, årgang, antalDøre);
+        this.harPartikelFilter = harPartikelFilter;
+        this.kmPrL = kmPrL;
+    }
+
+    public int getHarPartikelFilter() {
+        return harPartikelFilter;
+    }
+
+    public void setHarPartikelFilter(int harPartikelFilter) {
+        this.harPartikelFilter = harPartikelFilter;
+    }
+
+    public int getKmPrL() {
+        return kmPrL;
+    }
+
+    public void setKmPrL(int kmPrL) {
+        this.kmPrL = kmPrL;
+    }
+
+    @Override
+    public String toString() {
+        return "Dieselbil{" +
+                "regNr=" + regNr +
+                ", mærke=" + mærke +
+                ", model=" + model +
+                ", årgang=" + årgang +
+                ", antalDøre=" + antalDøre +
+                ", harPartikelFilter=" + harPartikelFilter +
+                ", kmPrL=" + kmPrL +
+                '}';
+    }
+
+    @Override
+    public double beregnGrønEjerafgift() {
+        return 0;
+    }
+}
